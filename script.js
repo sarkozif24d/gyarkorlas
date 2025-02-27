07c023d27f237764bc877c51569a2f83425daf33
sarkozif24d <sarkozif.24d@acsjszki.hu>
2025-02-27 08:41:17 +0100

    bruh

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ console.log(height);  //  ->  undefined
 console.log(weight);  //  ->  Uncaught  ReferenceError:  weight  is  not  defined
 let height;
 console.log(height); // -> undefined
+var height;
+var height;
+console.log(height)
