315d27d60ec91a90085cadaa6bdf2f475fa26077
sarkozif24d <sarkozif.24d@acsjszki.hu>
2025-02-27 08:45:23 +0100

    bruh3

--- script.js
+++ script.js
@@ -5,4 +5,17 @@ let height;
 console.log(height); // -> undefined
 var height;
 var height;
-console.log(height)
+console.log(height) // -> undefined
+let  height;
+let  height;  //  ->  Uncaught  SyntaxError:  Identifier  'height'  has  already  been  declared
+console.log(height);
+let  height  =  180;
+let  anotherHeight  =  height;
+let  weight;
+console.log(height);  //  ->  180
+console.log(anotherHeight);  //  ->  180
+weight  =  70;    
+console.log(weight);  //  ->  70
+let  height  =  180;
+console.log(height);  //  ->  180
+console.log("height");  //  ->  height
