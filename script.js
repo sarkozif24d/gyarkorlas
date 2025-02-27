14f2d2ebf6d9c2a4a72aafc03611e3d8932f27b8
sarkozif24d <sarkozif.24d@acsjszki.hu>
2025-02-27 14:11:24 +0100

    bruh4

--- script.js
+++ script.js
@@ -1,21 +1,87 @@
-var  height;
-console.log(height);  //  ->  undefined
-console.log(weight);  //  ->  Uncaught  ReferenceError:  weight  is  not  defined
-let height;
-console.log(height); // -> undefined
-var height;
-var height;
-console.log(height) // -> undefined
-let  height;
-let  height;  //  ->  Uncaught  SyntaxError:  Identifier  'height'  has  already  been  declared
-console.log(height);
-let  height  =  180;
-let  anotherHeight  =  height;
-let  weight;
-console.log(height);  //  ->  180
-console.log(anotherHeight);  //  ->  180
-weight  =  70;    
-console.log(weight);  //  ->  70
-let  height  =  180;
-console.log(height);  //  ->  180
-console.log("height");  //  ->  height
+/*
+* Operátorok
+*/
+
+
+// (number, number) -> number
+console.log(2 + 3)
+
+/* 
+       Operandusok száma szerint:
+       1:unary
+       2:binary
+       3: ternary
+    - operátor lexikális elhelyezkedése
+      előtte. perfix
+      közötte: infix
+      utána: postfix
+    - hajt-e végre mellékhatás? (side effect)
+
+    igen
+
+    nem     
+
+
+
+
+
+
+/*
+* Aritnetika
+* (number, number) -> number
+*/
+
+// +,-,*,/,%
+console.log(2 + 4);
+console.log(2 - 4);
+console.log(2 * 4);
+console.log(2 / 4);
+console.log(3 % 7);
+
+
+/*
+* Összehaspmlító
+* (any, any) -> boolean
+*/
+//==
+console.log(2 == 1);
+// !=
+console.log(2 != 2);
+
+// ===
+console.log(1 === true);
+console.log(1 !== true);
+console.log('János' === 'József');
+
+// >, <, >=, <=
+console.log(3 > 5);
+
+/*
+* Logikai
+* (boolean, boolean)  -> boolean
+*/
+
+// logikai És
+console.log(true && true)
+
+
+// logai vagy
+console.log(true || false)
+
+// negálás
+console.log(!true)
+
+/** 
+ * String operátor
+ */
+// konkaktenálás
+console.log('boldog szüli napot' + 'darkmasked2');
+// -> 'boldog szüli napot darkmasked2'
+
+// conditinal
+/*
+* (bool, any, any) ->
+*Conditinal
+*/
+
+console.log(true ? 'elsö' : 'második');
